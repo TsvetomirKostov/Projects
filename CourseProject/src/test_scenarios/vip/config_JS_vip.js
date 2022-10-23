e036590e0d84279a918a11214fb504ef9206eaac
Tsvetomir Kostov <106927094+TsvetomirKostov@users.noreply.github.com>
2022-10-23 19:09:13 +0300

    Add files via upload

---
+++ CourseProject/src/test_scenarios/vip/config_JS_vip.js
@@ -0,0 +1,44 @@
+const fs = require(`fs`);
+const data_restricted = fs.readFileSync(`./config_vip.json`);
+
+
+// Четене на JSON файла
+const data_vip = fs.readFileSync(`./config_vip.json`);
+const words = JSON.parse(data_vip);
+class Person {
+    function() {
+     
+        // assigning  parameter values to the calling object
+        this.name = userName,
+        this.age = age,
+        this.gender = gender,
+        this.Friends = numberOfFriends,
+        this.Photos = numberOfPhotos,
+        this.Videos = numberOfVideos,
+        this.Type = accountType,
+        this.Credit = accountCredit,
+        this.Account = isAccountRestricted
+    }
+}
+console.log(words);
+
+// Добавяне на функционалност според вида акаунт
+
+// Добавяне на приятел
+const addNewFriend =  fs.readFileSync(`./config_vip.json`);
+const file = addNewFriend + ``;
+const jsObject = JSON.parse(file);
+console.log(jsObject);
+
+
+// Добавяне на снимки
+const addNewPhoto =  fs.readFileSync(`./config_vip.json`);
+const filePhoto = addNewPhoto + ``;
+const jsObjectPhoto = JSON.parse(filePhoto);
+console.log(jsObjectPhoto);
+
+// Добавяне на видео 
+const addNewVideo =  fs.readFileSync(`./config_vip.json`);
+const fileVideo = addNewVideo + ``;
+const jsObjectVideo = JSON.parse(fileVideo);
+console.log(jsObjectVideo);
